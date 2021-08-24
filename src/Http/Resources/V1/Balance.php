175e553bb050a0415e90021b619da656d352931f
Felix Huber <felix.a.huber@gmx.net>
2021-08-24 22:54:50 +0200

    Introduce Character Resource

---
+++ src/Http/Resources/V1/Balance.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Seatplus\Api\Http\Resources\V1;
+
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Balance extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            $this->balance,
+        ];
+    }
+
+}
---
+++ src/Http/Resources/V1/Character.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Seatplus\Api\Http\Resources\V1;
+
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Character extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'character_id' => $this->character_id,
+            'name' => $this->name,
+            'birthday' => $this->birthday,
+            'balance' => Balance::collection($this->whenLoaded('balance')),
+            'total_sp' => $this->total_sp,
+            'unallocated_sp' => $this->unallocated_sp,
+        ];
+    }
+
+}
