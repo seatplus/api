3056d1987aa823f442ea65343f857f891f26dbba
Felix Huber <felix.a.huber@gmx.net>
2021-08-28 20:31:02 +0200

    Use Character Resource

--- src/Http/Controllers/Api/V1/User.php
+++ src/Http/Controllers/Api/V1/User.php
@@ -9,7 +9,8 @@ class User
 {
     public function __invoke(int $user_id)
     {
-        $user = UserModel::query()->with('characters')->find($user_id);
+
+        $user = UserModel::query()->with(['characters.balance', 'main_character.balance'])->find($user_id);
 
         return \Seatplus\Api\Http\Resources\V1\Users\User::make($user);
     }
